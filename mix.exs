fa58e0044228390d623de34917caa7446843843b
vincent-guo <vincent.weiguo@gmail.com>
2022-11-07 10:46:40 +0800

    Enable LiveDashboard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Chirp.MixProject do
   def application do
     [
       mod: {Chirp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
