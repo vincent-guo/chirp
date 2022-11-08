d8d8f24da32cc5415d86ab8179445bd4f5983b2e
vincent-guo <vincent.weiguo@gmail.com>
2022-11-08 19:52:02 +0800

    Add like and repost handle

--- lib/chirp_web/live/post_live/post_component.ex
+++ lib/chirp_web/live/post_live/post_component.ex
@@ -1,4 +1,13 @@
 defmodule ChirpWeb.PostLive.PostComponent do
   use ChirpWeb, :live_component
 
+  def handle_event("like", _, socket) do
+    Chirp.Timeline.inc_likes(socket.assigns.post)
+    {:noreply, socket}
+  end
+
+  def handle_event("repost", _, socket) do
+    Chirp.Timeline.inc_reposts(socket.assigns.post)
+    {:noreply, socket}
+  end
 end
